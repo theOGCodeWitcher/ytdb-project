90d8d1b9bef6f1d1b3c302304abd25d198ec695f
theOGCodeWitcher <rahulsingh97r@gmail.com>
2023-11-06 14:58:20 -0500

    carousel controls mobile v3

--- src/components/CardCollectionSkeleton.tsx
+++ src/components/CardCollectionSkeleton.tsx
@@ -4,7 +4,7 @@ export const CardCollectionSkeleton = () => {
   const skeletonCards = 4;
 
   return (
-    <div className="flex mx-2 px-2 pb-2  md:pb-8 md:mx-16 overflow-x-auto gap-3 md:p-4 md:gap-8 no-scrollbar">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 mx-2 px-2 pb-2 md:pb-8 md:mx-16">
       {[...Array(skeletonCards)].map((_, index) => (
         <CardSkeleton key={index} />
       ))}
