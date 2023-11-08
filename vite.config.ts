771d45f6db2d4d19056db9d74fffd8674f9060e1
theOGCodeWitcher <rahulsingh97r@gmail.com>
2023-11-08 01:02:12 -0500

    dependency added

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,9 @@ import replace from "@rollup/plugin-replace";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ["prop-types"],
+    },
+  },
 });
